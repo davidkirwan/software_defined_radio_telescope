bc004cf9643280d2a52a90111bcbca395e409bf1
David Kirwan <dkirwan@tssg.org>
2014-10-14 15:48:59 +0100

    - Porting QBasic Jovian Emission Prediction tables to Ruby

---
+++ jovian-radio-emission-prediction/jupiter-io-decametric-prediction.rb
@@ -0,0 +1,31 @@
+####################################################################################################
+# Description: Script to calculate likely observation windows for Jovian emissions in the Decametric 
+# band (10 - 100 Meters).
+# Based on the QBasic program: http://www.spaceacademy.net.au/spacelab/projects/jovrad/jovrad.htm 
+# 
+# Author: David Kirwan https://github.com/davidkirwan/software_defined_radio_telescope
+####################################################################################################
+
+
+
+
+
+
+target_year = "2014"
+
+
+f = File.open("output.txt", "w")
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+pi = Math::PI
+kr = pi / 180
+
+
+
+
+
+
+
+
+f.close
