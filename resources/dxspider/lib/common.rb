7f4d4d633b1fe37ca3fb96b56ff7295d31e61ecd
David Kirwan <davidkirwanirl@gmail.com>
2015-05-09 03:37:28 +0000

    - Initial implementation of the dxspider client refs #23

---
+++ resources/dxspider/lib/common.rb
@@ -0,0 +1,11 @@
+####################################################################################################
+# @author       David Kirwan https://github.com/davidkirwan
+# @description  Common Task helper
+#
+# @date         2013-07-12
+####################################################################################################
+
+# Require the common libraries
+Dir[File.dirname(__FILE__) + "/common/*.rb"].each do |file|
+  require file
+end
